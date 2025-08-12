8d16da76a738f8747e5bd435a87f8b0bbe2691d7
rafika khatun <rafikakhatun607@gmail.com>
2025-08-12 23:17:29 +0530

    Add MongoDB connection setup with database and users collection

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const cors = require('cors')
 const port = 5070;
@@ -9,6 +10,38 @@ app.use(express.json());
 app.use(cors());
 
 
+// mongodb connection 
+// MongoDB connection 
+
+const uri = "mongodb+srv://rafikakhatun:Rafika786@cluster0.ybxfupc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+let usersCollection;
+
+async function connectDB() {
+
+    try{
+        await client.connect();
+        console.log("connect to mongoDB")
+
+        const db = client.db("myDb"); // create database 
+        usersCollection = db.collection("userTable"); // create database collection 
+    }catch(error){
+        console.log("mongoDB connection faild",error);
+    }
+
+
+    
+}
+connectDB()
+
+
 
 
 
@@ -16,7 +49,7 @@ app.use(cors());
 //test route 
 
 app.get("/",(req,res)=>{
-    res.send("server is running")
+    res.send("server is running now")
 });
 
 
